5aa94001c69f12c34c6c984059fc1f1f71fc63e3
Nataliia Tolokontseva <nataliia_tolokontseva@epam.com>
2020-08-10 11:41:30 +0300

    Update task2.1 and 1.1 with better solutions

--- homework-1/task-1-1.js
+++ homework-1/task-1-1.js
@@ -6,25 +6,7 @@ const stdinStream = process.stdin;
 stdinStream.setEncoding("utf-8");
 
 console.log("Waiting for input...");
-stdinStream.on("data", reverseLine);
-
-function reverseLine(str) {
-    const stringWithoutLineEnd = str.replace(/[\r\n]/g, "");
-    const charArray = stringWithoutLineEnd.split("");
-
-    for (let i = 0; i < charArray.length / 2; i++) {
-        swap(charArray, i, charArray.length - i - 1);
-    }
-
-    console.log("Reversed line: ", charArray.join(""));
+stdinStream.on("data", (str) => {
+    console.log("Reversed line: ", str.split("").reverse().join(""));
     console.log("\nWaiting for input...");
-}
-
-function swap(arr, i, j) {
-    const tmp = arr[j];
-    arr[j] = arr[i];
-    arr[i] = tmp;
-}
-
-//question 1: is there better ways to read stdin?
-//question 2: am I supposed to write functions myself or usage of libraries (e.g. lodash) is preferable?
+});
--- homework-2/task-2-1.ts
+++ homework-2/task-2-1.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import winston from "winston";
-import expressWinston from "express-winston";
-import {Request, Response} from "express";
+import * as winston from "winston";
+import * as  expressWinston from "express-winston";
 import {v4 as uuidv4} from "uuid";
-import {find, findIndex, sortBy, defaultTo, filter, slice} from "lodash";
+import {sortBy} from "lodash";
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 type User = {
@@ -28,7 +27,11 @@ const MODERATOR: User = {
     age: 23,
     isDeleted: false,
 };
-const USER_COLLECTION: User[] = [ADMIN, MODERATOR];
+const USER_COLLECTION: Record<string, User> = {
+    [ADMIN.id]: ADMIN,
+    [MODERATOR.id]: MODERATOR
+};
+const DEFAULT_SLICE_LIMIT = 10;
 
 const app = express();
 const router = express.Router();
@@ -47,67 +50,53 @@ app.use(expressWinston.logger({
     meta: false,
 }));
 
-// query param: loginSubstring and limit
-// example: /user/suggest?limit=5&loginSubstring=har
-// returns array of found users
-router.get("/user/suggest", (
-    req: Request, res: Response<{ users: User[] }>
-) => {
-    const loginSubstring: string = defaultTo(req.query.loginSubstring, "")
-        .toString()
-        .toLowerCase();
-    const limit: number = +defaultTo(req.query.limit, "");
-    const findUserByLoginSubstr = (user: User) =>
-        user.login.toLowerCase().includes(loginSubstring);
-    const users: User[] = sortBy(
-        slice(
-            filter(USER_COLLECTION, findUserByLoginSubstr),
-            0, limit),
-        "login") as User[];
-    res.status(200).send({users});
-});
-
-router.get("/user/:id", (
-    req: Request, res: Response<User | { message: string }>
-) => {
-    const user = find(USER_COLLECTION, {id: req.params.id});
-    if (!user) {
+router.param("id", (req: express.Request, res: express.Response, next) => {
+    const { id } = req.params;
+    const userExist: boolean = Object.keys(USER_COLLECTION).includes(id);
+    if (!userExist) {
         res.status(404).send({
             message: `User with id ${req.params.id} is not found.`,
         });
     } else {
-        res.status(200).json(user);
+        next();
     }
 });
 
-router.put("/user/:id", (
-    req: Request, res: Response<User | { message: string }>
-) => {
-    const {age, login, password} = req.body;
-    const userIndex: number = findIndex(USER_COLLECTION,
-        {id: req.params.id},
-    );
-
-    if (userIndex === -1) {
-        const body = {
-            message: `User with id ${req.params.id} is not found.`,
-        };
-        res.status(404).send(body);
-    } else {
-        USER_COLLECTION[userIndex] = {
-            ...USER_COLLECTION[userIndex],
-            age,
-            login,
-            password,
-        };
-        res.status(200).send(USER_COLLECTION[userIndex]);
-    }
+// query param: loginSubstring and limit
+// example: /user/suggest?limit=5&loginSubstring=har
+// returns array of found users
+// NB! user/suggest should be always before user/:id
+router.get("/user/suggest", (req: express.Request, res: express.Response<{ users: User[] }>) => {
+    const { loginSubstring = "", limit } = req.query;
+    const lowLoginSubstring = (loginSubstring as string).toLowerCase();
+    const sliceLimit = limit ? +limit : DEFAULT_SLICE_LIMIT;
+    const findUserByLoginSubstr = (user: User) =>
+        user.login.toLowerCase().includes(lowLoginSubstring);
+    const users: User[] = sortBy(
+        Object.values(USER_COLLECTION).filter(findUserByLoginSubstr).slice(0, sliceLimit),
+        "login") as User[];
+    res.status(200).send({ users });
 });
 
-router.post("/user", (
-    req, res: Response<User>
-) => {
-    const {age = "", login = "", password = ""} = req.body;
+router.get("/user/:id", (req: express.Request, res: express.Response<User | { message: string }>) => {
+    const { id } = req.params;
+    res.status(200).json(USER_COLLECTION[id]);
+});
+
+router.put("/user/:id", (req: express.Request, res: express.Response<User | { message: string }>) => {
+    const { age, login, password } = req.body;
+    const { id } = req.params;
+    USER_COLLECTION[id] = {
+        ...USER_COLLECTION[id],
+        age,
+        login,
+        password,
+    };
+    res.status(200).send(USER_COLLECTION[id]);
+});
+
+router.post("/user", (req: express.Request, res: express.Response<User>) => {
+    const { age = 0, login = "", password = "" } = req.body;
     const user: User = {
         id: uuidv4(),
         login,
@@ -115,27 +104,19 @@ router.post("/user", (
         age,
         isDeleted: false,
     };
-    USER_COLLECTION.push(user);
+    USER_COLLECTION[user.id] = user;
     res.status(201).json(user);
 });
 
-router.delete("/user/:id", (
-    req: Request, res: Response<{ message: string }>
-) => {
-    const userIndex = findIndex(USER_COLLECTION, {id: req.params.id});
-    if (userIndex === -1) {
-        res.status(404).send({
-            message: `User with id ${req.params.id} is not found.`,
-        });
-    } else {
-        USER_COLLECTION[userIndex] = {
-            ...USER_COLLECTION[userIndex],
-            isDeleted: true,
-        };
-        res.status(200).send({
-            message: `User with id ${req.params.id} was deleted.`,
-        });
-    }
+router.delete("/user/:id", (req: express.Request, res: express.Response<{ message: string }>) => {
+    const { id } = req.params;
+    USER_COLLECTION[id] = {
+        ...USER_COLLECTION[id],
+        isDeleted: true,
+    };
+    res.status(200).send({
+        message: `User with id ${id} was deleted.`
+    });
 });
 
 app.use("/", router);
